9ac76a5a5e6b9e635dbd0065c98b103475514edf
showman228 <nekrasovaleksey.1999@gmail.com>
2026-06-28 21:29:18 +0300

    настроен файл main.py для запуска проекта

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from backend.app.database import init_db
+from backend.app.routers.users_routers import router as users_router
+from backend.app.config import settings
 
-app = FastAPI()
+app = FastAPI(title=settings.app_name)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.cors_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(users_router)
+
+@app.on_event("startup")
+def on_startup():
+    init_db()
 
 @app.get("/")
 def hello_world():
-    return {
-        "message": "Hello World!",
-    }
+    return {"message": "Hello World!"}
